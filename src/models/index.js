1058446d8cfa1426029e3789fc1329a5b5a80856
David <daviddemeramacias@gmail.com>
2026-07-30 16:01:32 -0500

    actualizar index.

--- src/models/index.js
+++ src/models/index.js
@@ -1,43 +1,25 @@
+
 import sequelize from '../config/database.js';
-import { initAsignatura } from './asignatura.model.js';
-import { initCarrera } from './carrera.model.js';
-import { initCarreraAsignatura } from './carreraAsignatura.model.js';
-import { initCurso } from './curso.model.js';
+
 import { initDocente } from './docente.model.js';
+import { initCurso } from './curso.model.js';
 import { initEstudiante } from './estudiante.model.js';
-import { initFacultad } from './facultad.model.js';
-import { initMatricula } from './matricula.model.js';
-import { initPeriodoAcademico } from './periodoAcademico.model.js';
+
+export const Docente = initDocente(sequelize);
+export const Curso = initCurso(sequelize);
+export const Estudiante = initEstudiante(sequelize);
 
 const models = {
-  Facultad: initFacultad(sequelize),
-  Carrera: initCarrera(sequelize),
-  Estudiante: initEstudiante(sequelize),
-  Asignatura: initAsignatura(sequelize),
-  CarreraAsignatura: initCarreraAsignatura(sequelize),
-  Docente: initDocente(sequelize),
-  PeriodoAcademico: initPeriodoAcademico(sequelize),
-  Curso: initCurso(sequelize),
-  Matricula: initMatricula(sequelize)
+  Docente,
+  Curso,
+  Estudiante
 };
 
 Object.values(models).forEach((model) => {
-  if (typeof model.associate === 'function') {
+  if (model.associate) {
     model.associate(models);
   }
 });
 
 export { sequelize };
-export const {
-  Facultad,
-  Carrera,
-  Estudiante,
-  Asignatura,
-  CarreraAsignatura,
-  Docente,
-  PeriodoAcademico,
-  Curso,
-  Matricula
-} = models;
-
 export default models;
