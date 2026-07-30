44c4160bbc2e8a61345ae89dc32a65626a85363e
Bernardo <yaguard1999@gmail.com>
2026-07-30 18:23:48 -0500

    modificar index conla ruta de asignatura

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import { Docente } from '../models/index.js';
 import docenteRoutes from './docentes.routes.js';
 import cursoRoutes from './curso.routes.js';
 import estudianteRoutes from './estudiante.routes.js';
-
+import asignaturaRoutes from './asignatura.routes.js';
 
 const router = Router();
 
@@ -11,6 +11,6 @@ const router = Router();
 router.use('/docentes', docenteRoutes);
 router.use('/cursos', cursoRoutes);
 router.use('/estudiantes', estudianteRoutes);
-
+router.use('/asignaturas', asignaturaRoutes);
 
 export default router;
