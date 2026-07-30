dcf7f0f4738a137956845981951eddc07c7a2408
Bernardo <yaguard1999@gmail.com>
2026-07-30 18:31:07 -0500

    inicializacion e imporatacion de asignatura

--- src/models/index.js
+++ src/models/index.js
@@ -4,15 +4,18 @@ import sequelize from '../config/database.js';
 import { initDocente } from './docente.model.js';
 import { initCurso } from './curso.model.js';
 import { initEstudiante } from './estudiante.model.js';
+import { initAsignatura } from './asignatura.model.js';
 
 export const Docente = initDocente(sequelize);
 export const Curso = initCurso(sequelize);
 export const Estudiante = initEstudiante(sequelize);
+export const Asignatura = initAsignatura(sequelize);
 
 const models = {
   Docente,
   Curso,
-  Estudiante
+  Estudiante,
+  Asignatura,
 };
 
 Object.values(models).forEach((model) => {
