7b577ec9c5514891b72ea500559c50a7ebf07626
David <daviddemeramacias@gmail.com>
2026-07-30 16:08:18 -0500

    feat(routes): actualizar rutas en index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,16 @@
+import { Router } from 'express';
+import { Docente } from '../models/index.js';
+import docenteRoutes from './docentes.routes.js';
+import cursoRoutes from './curso.routes.js';
+import estudianteRoutes from './estudiante.routes.js';
 
-import cors from 'cors';
-import express from 'express';
-import config from './config/env.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import routes from './routes/index.js';
 
-const app = express();
+const router = Router();
 
-const corsOptions =
-  config.corsOrigin === '*'
-    ? { origin: '*' }
-    : { origin: config.corsOrigin.split(',').map((origin) => origin.trim()) };
 
-app.use(cors(corsOptions));
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ extended: true }));
+router.use('/docentes', docenteRoutes);
+router.use('/cursos', cursoRoutes);
+router.use('/estudiantes', estudianteRoutes);
 
-app.use('/api/v1', routes);
 
-app.use(notFoundHandler);
-app.use(errorHandler);
-
-export default app;
+export default router;
