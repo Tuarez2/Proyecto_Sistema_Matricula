d339e9709275e2f6ca18b616a61eff0803e828e9
Bernardo <yaguard1999@gmail.com>
2026-07-30 18:39:57 -0500

    inizializacion e importar carrera en index

--- src/models/index.js
+++ src/models/index.js
@@ -5,17 +5,18 @@ import { initDocente } from './docente.model.js';
 import { initCurso } from './curso.model.js';
 import { initEstudiante } from './estudiante.model.js';
 import { initAsignatura } from './asignatura.model.js';
-
+import { initCarrera } from './carrera.model.js';
 export const Docente = initDocente(sequelize);
 export const Curso = initCurso(sequelize);
 export const Estudiante = initEstudiante(sequelize);
 export const Asignatura = initAsignatura(sequelize);
-
+export const Carrera = initCarrera(sequelize);
 const models = {
   Docente,
   Curso,
   Estudiante,
   Asignatura,
+  Carrera,
 };
 
 Object.values(models).forEach((model) => {
